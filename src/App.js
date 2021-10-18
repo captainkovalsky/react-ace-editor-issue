95a4b15ff48b629203fb42d4cecce5907a3f1c84
Viktor Dzundza <vdzundza@protonmail.com>
2021-10-19 01:34:57 +0300

    edit two editors
    Signed-off-by: Viktor Dzundza <vdzundza@protonmail.com>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import 'ace-builds';
 import "ace-builds/src-noconflict/theme-tomorrow_night";
 import "ace-builds/src-noconflict/mode-javascript";
 import AceEditor from 'react-ace';
 
 const init = `
-function test() {
-  return null;
-}
+//edit text
 `;
+
+const styles = { maxHeight: '200px', display: 'inline-block' };
+
+async function long(text) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(text + `\r\n //updated ${new Date().getTime()}`);
+    }, 5 * 1000);
+  })
+}
+
+function useAction() {
+  const doSomething = async (aceEditor) => {
+    const value = aceEditor.editor.getValue();
+    const result = await long(value);
+    aceEditor.editor.setValue(result, 1);
+  }
+
+  return {
+    doSomething
+  }
+}
+
 export default function App() {
   const [text, setText] = useState(init);
   const [text2, setText2] = useState(init);
+
+  const { doSomething } = useAction();
+
+  const editorRefs = useRef({});
+
+  const changeText = useCallback((n) => (t) => {
+    n === 1 ? setText(t) : setText2(t);
+  }, [setText2, setText]
+  );
+
+  const changeText1 = useCallback(changeText(1), [changeText]);
+  const changeText2 = useCallback(changeText(2), [changeText]);
 
   return (
     <div>
@@ -23,10 +56,17 @@ export default function App() {
         theme="tomorrow_night"
         name="editor1"
         value={text}
+        onChange={changeText1}
+        ref={ref => {
+          editorRefs.current['1'] = ref;
+        }}
         fontSize={14}
-        style={{ maxHeight: '200px' }}
+        style={styles}
         editorProps={{ $blockScrolling: true }}
       />
+      <button onClick={() => {
+        doSomething(editorRefs.current['1']);
+      }}>Update text Editor 1</button>
       <hr />
       <AceEditor
         mode="javascript"
@@ -34,9 +74,17 @@ export default function App() {
         name="editor2"
         value={text2}
         fontSize={14}
-        style={{ maxHeight: '200px' }}
+        onChange={changeText2}
+        style={styles}
+        ref={ref => {
+          editorRefs.current['2'] = ref;
+        }}
         editorProps={{ $blockScrolling: true }}
       />
+      <button onClick={() => {
+        doSomething(editorRefs.current['2']);
+      }}>Update text Editor 2</button>
+
     </div>
   );
 }

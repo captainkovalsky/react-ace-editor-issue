2451005e520053baf9a5c22124177048e7e59e2a
Viktor Dzundza <vdzundza@protonmail.com>
2021-10-19 00:51:50 +0300

    prepare reproduce repo
    Signed-off-by: Viktor Dzundza <vdzundza@protonmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'ace-builds';
+import "ace-builds/src-noconflict/theme-tomorrow_night";
+import "ace-builds/src-noconflict/mode-javascript";
 import AceEditor from 'react-ace';
 
-console.log('ace', ace);
+const init = `
+function test() {
+  return null;
+}
+`;
 export default function App() {
+  const [text, setText] = useState(init);
+  const [text2, setText2] = useState(init);
+
   return (
     <div>
       <h1>Hello AceEditor!</h1>
@@ -11,14 +20,22 @@ export default function App() {
 
       <AceEditor
         mode="javascript"
-        theme="tomorrow"
-        name="editor"
-        value={''}
+        theme="tomorrow_night"
+        name="editor1"
+        value={text}
         fontSize={14}
-        setOptions={{
-          useWorker: false,
-        }}
-        editorProps={{ $blockScrolling: true, use }}
+        style={{ maxHeight: '200px' }}
+        editorProps={{ $blockScrolling: true }}
+      />
+      <hr />
+      <AceEditor
+        mode="javascript"
+        theme="tomorrow_night"
+        name="editor2"
+        value={text2}
+        fontSize={14}
+        style={{ maxHeight: '200px' }}
+        editorProps={{ $blockScrolling: true }}
       />
     </div>
   );
